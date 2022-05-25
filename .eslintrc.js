5c74fe30905c1142b5d9a8c3405c95a8a13f2fad
yuanyelv <287142032@qq.com>
2022-05-25 19:32:25 +0800

    commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+/*
+ * @Author: yuanyelv 287142032@qq.com
+ * @Date: 2022-05-25 18:36:53
+ * @LastEditors: yuanyelv 287142032@qq.com
+ * @LastEditTime: 2022-05-25 18:43:14
+ * @FilePath: \vite-vue3-01\.eslintrc.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:vue/vue3-strongly-recommended',
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
